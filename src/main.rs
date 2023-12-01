9c40e3b7242e208e0c3f84ba20863a523db7055d
Mechti <mechti.oezdogan@exxeta.com>
2023-12-01 15:57:42 +0100

    do part 2 but this time both :)

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,68 @@ use std::io::{BufRead, BufReader};
 fn main() {
     let file = std::fs::File::open("input.txt").unwrap();
     let buf = BufReader::new(file);
-    let numbers = [
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ];
-    let sum = buf
+    let (sum_without, sum_with) = buf
         .lines()
         .filter_map(|l| {
             let line = l.unwrap();
-            let val = get_combined_digits(line.clone(), numbers);
-            println!("value for line {} is {}", line.clone(), val.unwrap_or(0));
-            val
+            let no_words = get_combined_digits(&line, false);
+            let words = get_combined_digits(&line, true);
+            println!(
+                "NOWORDS value for line {} is {}",
+                line,
+                no_words.unwrap_or(0)
+            );
+            println!("value for line {} is {}", line, words.unwrap_or(0));
+            Some((no_words?, words?))
         })
-        .sum::<i32>();
-    println!("sum is {}", sum);
+        .fold((0, 0), |(sum_without, sum_with), (no_words, words)| {
+            (sum_without + no_words, sum_with + words)
+        });
+    println!("sum without is {}", sum_without);
+    println!("sum with is {}", sum_with);
 }
 
-fn get_combined_digits(line: String, numbers: [(&str, i32); 9]) -> Option<i32> {
+fn get_combined_digits(
+    line: &str,
+    respect_words: bool
+) -> Option<i32> {
     let mut digits = String::new();
-    digits.push(
-        get_first_digit_in_chars(line.clone(), numbers)?
-            .to_string()
-            .parse()
-            .ok()?,
-    );
-    digits.push(
-        get_last_digit_in_chars(line.clone(), numbers)?
-            .to_string()
-            .parse()
-            .ok()?,
-    );
+
+    if respect_words {
+        let numbers = [
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9),
+        ];
+        digits.push(
+            get_first_digit_in_chars(line, numbers)?
+                .to_string()
+                .parse()
+                .ok()?,
+        );
+        digits.push(
+            get_last_digit_in_chars(line, numbers)?
+                .to_string()
+                .parse()
+                .ok()?,
+        );
+    } else {
+        digits.push(
+            get_first_digit_and_index(line)?
+                .1
+                .to_string()
+                .parse()
+                .ok()?,
+        );
+        digits.push(get_last_digit_and_index(line)?.1.to_string().parse().ok()?);
+    }
+
     if !digits.is_empty() {
         Some(digits.parse().ok()?)
     } else {
@@ -47,8 +72,8 @@ fn get_combined_digits(line: String, numbers: [(&str, i32); 9]) -> Option<i32> {
     }
 }
 
-fn get_first_digit_in_chars(line: String, numbers: [(&str, i32); 9]) -> Option<i32> {
-    let index_and_digit = line.chars().enumerate().find(|(_, c)| c.is_ascii_digit())?;
+fn get_first_digit_in_chars(line: &str, numbers: [(&str, i32); 9]) -> Option<i32> {
+    let index_and_digit = get_first_digit_and_index(line)?;
     let lowest_index = numbers
         .iter()
         .filter_map(|(n, _)| line.match_indices(*n).min_by_key(|(i, _)| *i))
@@ -62,9 +87,18 @@ fn get_first_digit_in_chars(line: String, numbers: [(&str, i32); 9]) -> Option<i
     Some(index_and_digit.1.to_digit(10)? as i32)
 }
 
-fn get_last_digit_in_chars(line: String, numbers: [(&str, i32); 9]) -> Option<i32> {
+fn get_first_digit_and_index(line: &str) -> Option<(usize, char)> {
+    line.chars().enumerate().find(|(_, c)| c.is_ascii_digit())
+}
+
+fn get_last_digit_and_index(line: &str) -> Option<(usize, char)> {
     let digit = line.chars().rev().find(|c| c.is_ascii_digit())?;
     let index_of_digit = line.chars().position(|c| c == digit)?;
+    Some((index_of_digit, digit))
+}
+
+fn get_last_digit_in_chars(line: &str, numbers: [(&str, i32); 9]) -> Option<i32> {
+    let (index_of_digit, digit) = get_last_digit_and_index(line)?;
     let highest_index = numbers
         .iter()
         .filter_map(|(n, _)| line.match_indices(*n).max_by_key(|(i, _)| *i))
