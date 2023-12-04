84ea93d54170fa5662f9723de241726bebc75b34
Mechti <mechti.oezdogan@exxeta.com>
2023-12-04 09:46:26 +0100

    open thread for each line (it's slower?)

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,77 @@
 use std::io::{BufRead, BufReader};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
     let file = std::fs::File::open("input.txt").unwrap();
     let buf = BufReader::new(file);
-    let (sum_without, sum_with) = buf
-        .lines()
-        .filter_map(|l| {
-            let line = l.unwrap();
-            let no_words = get_combined_digits(&line, false);
-            let words = get_combined_digits(&line, true);
-            println!(
-                "NOWORDS value for line {} is {}",
-                line,
-                no_words.unwrap_or(0)
-            );
-            println!("value for line {} is {}", line, words.unwrap_or(0));
-            Some((no_words?, words?))
-        })
-        .fold((0, 0), |(sum_without, sum_with), (no_words, words)| {
-            (sum_without + no_words, sum_with + words)
+
+    let lines = buf.lines();
+
+    let no_word_sum = Arc::new(Mutex::new(0));
+    let word_sum = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for line in lines {
+        let no_word_sum = Arc::clone(&no_word_sum);
+        let word_sum = Arc::clone(&word_sum);
+        let handle = thread::spawn(move || {
+            let line = line.unwrap();
+            if let Some(no_words) = get_combined_digits(&line, get_first_and_last_digits_only) {
+                let mut sum =  no_word_sum.lock().unwrap();
+                *sum += no_words;
+            }
+            if let Some(words) = get_combined_digits(&line, get_first_and_last_with_words) {
+                let mut sum =word_sum.lock().unwrap();
+                *sum += words;
+            }
         });
-    println!("sum without is {}", sum_without);
-    println!("sum with is {}", sum_with);
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("sum without is {}", *no_word_sum.lock().unwrap());
+    println!("sum with is {}", *word_sum.lock().unwrap());
 }
 
-fn get_combined_digits(
-    line: &str,
-    respect_words: bool
-) -> Option<i32> {
+fn get_combined_digits<F>(line: &str, f: F) -> Option<i32>
+where
+    F: Fn(&str) -> Option<(i32, i32)>,
+{
     let mut digits = String::new();
-
-    if respect_words {
-        let numbers = [
-            ("one", 1),
-            ("two", 2),
-            ("three", 3),
-            ("four", 4),
-            ("five", 5),
-            ("six", 6),
-            ("seven", 7),
-            ("eight", 8),
-            ("nine", 9),
-        ];
-        digits.push(
-            get_first_digit_in_chars(line, numbers)?
-                .to_string()
-                .parse()
-                .ok()?,
-        );
-        digits.push(
-            get_last_digit_in_chars(line, numbers)?
-                .to_string()
-                .parse()
-                .ok()?,
-        );
-    } else {
-        digits.push(
-            get_first_digit_and_index(line)?
-                .1
-                .to_string()
-                .parse()
-                .ok()?,
-        );
-        digits.push(get_last_digit_and_index(line)?.1.to_string().parse().ok()?);
+    let extracted_vals = f(line);
+    if let Some((first, last)) = extracted_vals {
+        digits.push(first.to_string().parse().ok()?);
+        digits.push(last.to_string().parse().ok()?);
+        return digits.parse::<i32>().ok();
     }
+    None
+}
 
-    if !digits.is_empty() {
-        Some(digits.parse().ok()?)
-    } else {
-        None
-    }
+fn get_first_and_last_digits_only(line: &str) -> Option<(i32, i32)> {
+    Some((
+        get_first_digit_and_index(line)?.1.to_digit(10)? as i32,
+        get_last_digit_and_index(line)?.1.to_digit(10)? as i32,
+    ))
+}
+
+fn get_first_and_last_with_words(line: &str) -> Option<(i32, i32)> {
+    let numbers = [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+    Some((
+        get_first_digit_in_chars(line, numbers)?,
+        get_last_digit_in_chars(line, numbers)?,
+    ))
 }
 
 fn get_first_digit_in_chars(line: &str, numbers: [(&str, i32); 9]) -> Option<i32> {
@@ -111,3 +113,4 @@ fn get_last_digit_in_chars(line: &str, numbers: [(&str, i32); 9]) -> Option<i32>
     }
     Some(digit.to_digit(10)? as i32)
 }
+
